0625f82474d8e48172ca76d856d4eae32b0e310b
Luca Soldaini <lucas@allenai.org>
2022-12-26 13:09:53 -0800

    updated queries to include citations

--- queries/paragraphs_acl.sql
+++ queries/paragraphs_acl.sql
@@ -1,79 +1,174 @@
+-- UNLOAD (
+--     SELECT
+--         id,
+--         MAX(title) AS title,
+--         MAX(abstract) AS abstract,
+--         MAX(year) AS year,
+--         NORMALIZE(
+--             ARRAY_JOIN(
+--                 ARRAY_AGG(paragraph), chr(13) || chr(13)
+--             )
+--         ) AS full_text
+--     FROM (
+--         SELECT
+--             id,
+--             year,
+--             title,
+--             abstract,
+--             fields_of_study,
+--             SUBSTR(full_text, paralocs[1], paralocs[2] - paralocs[1] + 1) AS paragraph
+--         FROM (
+--             SELECT
+--                 id,
+--                 year,
+--                 title,
+--                 abstract,
+--                 full_text,
+--                 ARRAY_JOIN(fields_of_study, ',') AS fields_of_study,
+--                 ARRAY_SORT(
+--                     ARRAY_DISTINCT(
+--                         TRANSFORM(
+--                             all_paralocs,
+--                             x -> (
+--                                 ARRAY [CAST(JSON_EXTRACT(x, '$.start') AS INT)] ||
+--                                 ARRAY [CAST(JSON_EXTRACT(x, '$.end') AS INT)]
+--                             )
+--                         )
+--                     )
+--                 ) AS all_paralocs
+--             FROM (
+--                 SELECT
+--                     spr.id,
+--                     spr.metadata.publication_date.year AS year,
+--                     spr.metadata.title AS title,
+--                     spr.metadata.abstract AS abstract,
+--                     CAST (
+--                         JSON_PARSE(spr.metadata.fields_of_study) AS ARRAY(varchar)
+--                     ) AS fields_of_study,
+--                     spr.content.grobid.contents AS full_text,
+--                     CAST(
+--                         JSON_PARSE(spr.content.grobid.annotations.paragraph)
+--                         AS ARRAY(json)
+--                     ) || CAST(
+--                         JSON_PARSE(spr.content.grobid.annotations.section_header)
+--                         AS ARRAY(json)
+--                     ) AS all_paralocs
+--                 FROM (
+--                     SELECT corpus_paper_id AS id
+--                     FROM content_ext.paper_sources
+--                     WHERE source = 'ACL'
+--                 ) AS corpus_ext
+--                 INNER JOIN s2orc_papers.releases AS spr
+--                     ON spr.id = corpus_ext.id
+--                 WHERE
+--                     year=2022 AND
+--                     month=12 AND
+--                     day=11 AND
+--                     metadata.fields_of_study IS NOT NULL
+--             )
+--             WHERE CONTAINS(fields_of_study, 'Computer Science')
+--         )
+--         CROSS JOIN UNNEST(all_paralocs) AS t(paralocs)
+--     )
+--     GROUP BY id
+-- )
+-- TO 's3://ai2-s2-lucas/s2orc_20221211/acl_s2orc/'
+-- WITH (
+--     format='JSON',
+--     compression='GZIP'
+-- )
+
 UNLOAD (
     SELECT
         id,
-        MAX(title) AS title,
-        MAX(abstract) AS abstract,
-        MAX(year) AS year,
-        NORMALIZE(
-            ARRAY_JOIN(
-                ARRAY_AGG(paragraph), chr(13) || chr(13)
-            )
-        ) AS full_text
+        year,
+        title,
+        abstract,
+        fields_of_study,
+        citations,
+        ARRAY_JOIN(
+            TRANSFORM(
+                all_paralocs,
+                paralocs -> NORMALIZE(
+                    SUBSTR(
+                        full_text,
+                        paralocs[1],
+                        paralocs[2] - paralocs[1] + 1
+                    )
+                )
+            ),
+        -- add two newlines between paragraphs
+        chr(13) || chr(13)
+        ) AS full_text,
+        -- make 3 partitions for smaller output files
+        floor(rand() * 3) AS part_id
     FROM (
         SELECT
             id,
             year,
             title,
             abstract,
-            fields_of_study,
-            SUBSTR(full_text, paralocs[1], paralocs[2] - paralocs[1] + 1) as paragraph
-        FROM (
-            SELECT
-                id,
-                year,
-                title,
-                abstract,
-                full_text,
-                ARRAY_JOIN(fields_of_study, ',') as fields_of_study,
-                ARRAY_SORT(
-                    ARRAY_DISTINCT(
-                        TRANSFORM(
-                            all_paralocs,
-                            x -> (
-                                ARRAY [CAST(JSON_EXTRACT(x, '$.start') AS INT)] ||
-                                ARRAY [CAST(JSON_EXTRACT(x, '$.end') AS INT)]
-                            )
+            full_text,
+            ARRAY_JOIN(fields_of_study, ',') AS fields_of_study,
+            ARRAY_SORT(
+                ARRAY_DISTINCT(
+                    TRANSFORM(
+                        all_paralocs,
+                        x -> (
+                            ARRAY [CAST(JSON_EXTRACT(x, '$.start') AS INT)] ||
+                            ARRAY [CAST(JSON_EXTRACT(x, '$.end') AS INT)]
                         )
                     )
-                ) AS all_paralocs
+                )
+            ) AS all_paralocs,
+            FILTER(
+                TRANSFORM(
+                    bib_entries,
+                    x -> CAST(
+                        JSON_EXTRACT(x, '$.attributes.matched_paper_id')
+                        AS BIGINT
+                    )
+                ),
+                x -> IF(x IS NULL, False, True)
+            ) AS citations
+        FROM (
+            SELECT
+                spr.id,
+                spr.metadata.publication_date.year AS year,
+                spr.metadata.title AS title,
+                spr.metadata.abstract AS abstract,
+                CAST (
+                    JSON_PARSE(spr.metadata.fields_of_study) AS ARRAY(varchar)
+                ) AS fields_of_study,
+                spr.content.grobid.contents AS full_text,
+                CAST(
+                    JSON_PARSE(spr.content.grobid.annotations.paragraph)
+                    AS ARRAY(json)
+                ) || CAST(
+                    JSON_PARSE(spr.content.grobid.annotations.section_header)
+                    AS ARRAY(json)
+                ) AS all_paralocs,
+                CAST(
+                    JSON_PARSE(spr.content.grobid.annotations.bib_entry)
+                    AS ARRAY(json)
+                ) as bib_entries
             FROM (
-                SELECT
-                    spr.id,
-                    spr.metadata.publication_date.year as year,
-                    spr.metadata.title as title,
-                    spr.metadata.abstract as abstract,
-                    CAST (
-                        json_parse(spr.metadata.fields_of_study) AS array(varchar)
-                    ) AS fields_of_study,
-                    spr.content.grobid.contents as full_text,
-                    CAST(
-                        json_parse(spr.content.grobid.annotations.paragraph)
-                        AS array(json)
-                    ) || CAST(
-                        json_parse(spr.content.grobid.annotations.section_header)
-                        AS array(json)
-                    ) AS all_paralocs
-                FROM (
-                    SELECT corpus_paper_id as id
-                    FROM content_ext.paper_sources
-                    WHERE source = 'ACL'
-                ) AS corpus_ext
-                INNER JOIN s2orc_papers.releases AS spr
-                    ON spr.id = corpus_ext.id
-                WHERE
-                    year=2022 AND
-                    month=12 AND
-                    day=11 AND
-                    metadata.fields_of_study IS NOT NULL
-            )
-            WHERE CONTAINS(fields_of_study, 'Computer Science')
+                SELECT corpus_paper_id AS id
+                FROM content_ext.paper_sources
+                WHERE source = 'ACL'
+            ) AS corpus_ext
+            INNER JOIN s2orc_papers.releases AS spr
+                ON spr.id = corpus_ext.id
+            WHERE
+                spr.year=2022 AND
+                spr.month=12 AND
+                spr.day=11
         )
-        CROSS JOIN UNNEST(all_paralocs) as t(paralocs)
     )
-    GROUP BY id
 )
-TO 's3://ai2-s2-lucas/s2orc_20221211/acl_s2orc/'
+TO 's3://ai2-s2-lucas/s2orc_20221211/acl_content_cits/'
 WITH (
     format='JSON',
-    compression='GZIP'
+    compression='GZIP',
+    partitioned_by = ARRAY['part_id']
 )
--- queries/paragraphs_cs.sql
+++ queries/paragraphs_cs.sql
@@ -1,74 +1,88 @@
 UNLOAD (
     SELECT
         id,
-        MAX(title) as title,
-        MAX(abstract) as abstract,
-        MAX(year) as year,
-        NORMALIZE(
-            ARRAY_JOIN(
-                ARRAY_AGG(paragraph), chr(13) || chr(13)
-            )
+        year,
+        title,
+        abstract,
+        fields_of_study,
+        citations,
+        ARRAY_JOIN(
+            TRANSFORM(
+                all_paralocs,
+                paralocs -> NORMALIZE(
+                    SUBSTR(
+                        full_text,
+                        paralocs[1],
+                        paralocs[2] - paralocs[1] + 1
+                    )
+                )
+            ),
+        -- add two newlines between paragraphs
+        chr(13) || chr(13)
         ) AS full_text,
         -- make 30 partitions for smaller output files
-        floor(rand() * 30) as part_id
+        floor(rand() * 30) AS part_id
     FROM (
         SELECT
             id,
             year,
             title,
             abstract,
-            fields_of_study,
-            SUBSTR(full_text, paralocs[1], paralocs[2] - paralocs[1] + 1) as paragraph
+            full_text,
+            ARRAY_JOIN(fields_of_study, ',') AS fields_of_study,
+            ARRAY_SORT(
+                ARRAY_DISTINCT(
+                    TRANSFORM(
+                        all_paralocs,
+                        x -> (
+                            ARRAY [CAST(JSON_EXTRACT(x, '$.start') AS INT)] ||
+                            ARRAY [CAST(JSON_EXTRACT(x, '$.end') AS INT)]
+                        )
+                    )
+                )
+            ) AS all_paralocs,
+            FILTER(
+                TRANSFORM(
+                    bib_entries,
+                    x -> CAST(
+                        JSON_EXTRACT(x, '$.attributes.matched_paper_id')
+                        AS BIGINT
+                    )
+                ),
+                x -> IF(x IS NULL, False, True)
+            ) AS citations
         FROM (
             SELECT
                 id,
-                year,
-                title,
-                abstract,
-                full_text,
-                ARRAY_JOIN(fields_of_study, ',') as fields_of_study,
-                ARRAY_SORT(
-                    ARRAY_DISTINCT(
-                        TRANSFORM(
-                            all_paralocs,
-                            x -> (
-                                ARRAY [CAST(JSON_EXTRACT(x, '$.start') AS INT)] ||
-                                ARRAY [CAST(JSON_EXTRACT(x, '$.end') AS INT)]
-                            )
-                        )
-                    )
-                ) AS all_paralocs
-            FROM (
-                SELECT
-                    id,
-                    metadata.publication_date.year as year,
-                    metadata.title as title,
-                    metadata.abstract as abstract,
-                    CAST (
-                        json_parse(metadata.fields_of_study) AS array(varchar)
-                    ) AS fields_of_study,
-                    content.grobid.contents as full_text,
-                    CAST(
-                        json_parse(content.grobid.annotations.paragraph)
-                        AS array(json)
-                    ) || CAST(
-                        json_parse(content.grobid.annotations.section_header)
-                        AS array(json)
-                    ) AS all_paralocs
-                FROM s2orc_papers.releases
-                WHERE
-                    year=2022 AND
-                    month=12 AND
-                    day=11 AND
-                    metadata.fields_of_study IS NOT NULL
-            )
-            WHERE CONTAINS(fields_of_study, 'Computer Science')
+                metadata.publication_date.year AS year,
+                metadata.title AS title,
+                metadata.abstract AS abstract,
+                CAST (
+                    JSON_PARSE(metadata.fields_of_study) AS ARRAY(varchar)
+                ) AS fields_of_study,
+                content.grobid.contents AS full_text,
+                CAST(
+                    JSON_PARSE(content.grobid.annotations.paragraph)
+                    AS ARRAY(json)
+                ) || CAST(
+                    JSON_PARSE(content.grobid.annotations.section_header)
+                    AS ARRAY(json)
+                ) AS all_paralocs,
+                CAST(
+                    JSON_PARSE(content.grobid.annotations.bib_entry)
+                    AS ARRAY(json)
+                ) as bib_entries
+            FROM s2orc_papers.releases
+            WHERE
+                year=2022 AND
+                month=12 AND
+                day=11 AND
+                metadata.fields_of_study IS NOT NULL
         )
-        CROSS JOIN UNNEST(all_paralocs) as t(paralocs)
+        WHERE CONTAINS(fields_of_study, 'Computer Science')
     )
-    GROUP BY id
 )
-TO 's3://ai2-s2-lucas/s2orc_20221211/cs_content/'
+TO 's3://ai2-s2-lucas/s2orc_20221211/cs_content_cits/'
 WITH (
     format='JSON',
     compression='GZIP',
